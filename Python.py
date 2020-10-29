ed436efd0c81d2364138db8407b962b9a933781c
vaanigupta08 <preetigupta.mams@gmail.com>
2020-10-29 21:32:28 +0530

    first commit

---
+++ Python.py
@@ -0,0 +1,18 @@
+# PYTHON CODING
+print("Hello Python")
+apple= 25.02
+print (type (apple))
+print  (int (apple))
+name= 'Vaani'
+print (len (name))
+print (name. upper())
+
+## This is a multi-line comment
+print (''' I am a python learner
+I love doing python
+have a lot of interest ''')
+ 
+car= int(input('Enter a number'))
+tip= int(input('Enter second number'))
+Result= car+tip
+print(Result) 
