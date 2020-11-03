cf77b83a50d83719418a81c89a16aa9e20b0e0f7
VAANI GUPTA <73345388+vaanigupta08@users.noreply.github.com>
2020-11-03 17:43:01 +0530

    Add files via upload

---
+++ BIODATA+LIST.py
@@ -0,0 +1,66 @@
+# PYTHON CODING
+print("Hello Python")
+apple= 25.02
+print (type (apple))
+print  (int (apple))
+name= 'Vaani'
+print (len (name))
+print (name. upper())
+
+## This is a multi-line comment
+print (''' I am a python learner
+I love doing python
+have a lot of interest ''')
+ 
+'''car= int(input('Enter a number'))
+tip= int(input('Enter second number'))
+Result= car+tip
+print(Result)'''
+print (name [:5])
+
+# TODAY'S PYTHON CODING(30.10.20)
+
+## BIODATA OF VAANI GUPTA
+
+
+Name= 'Vaani Gupta'
+Grade= 'VIII'
+Section= 'H'
+School= 'Bal Bharati Public School'
+Hobbies= 'singing , dancing , python'
+DOB= '8th August'
+Age= '13 years'
+
+
+'{0}s BIODATA'
+
+
+print('''
+Name: {0}
+Grade: {1}
+Section: {2}
+School: {3}
+Hobbies: {4}
+DOB: {5}
+Age: {6}'''.format(Name , Grade , Section , School , Hobbies , DOB , Age ))
+
+
+print ('\n My name contains alphabets:' , name.isalpha())
+
+print ('\n My name consists of numbers too:' , name.isalnum())
+
+# TODAY'S CODING (3.11.20)
+
+Mylist = [85, 74, [200, 41, 23], 70, 52]
+
+print ('slicing:' , Mylist [1:3])
+print ('\n negative slicing:' , Mylist [-1:2])
+
+Mytuple = (63, 52, 87, 30, (72, 10), 540)
+
+print ('\n LETS SEE THE DATATYPE:' , type(Mytuple))
+
+class_5 = ['\n Rohini' , 'Rupali' , 'Tisya' , 'Purva' , 'Shaurya' , 'Shargul']
+
+class_5.append ('Shilpi')
+
