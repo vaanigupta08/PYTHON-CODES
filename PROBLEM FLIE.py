af83df13021a6e5180baf50d5264e39878c2499d
VAANI GUPTA <73345388+vaanigupta08@users.noreply.github.com>
2020-11-06 13:37:38 +0530

    Add files via upload

---
+++ PROBLEM FLIE.py
@@ -0,0 +1,19 @@
+# MY FIRST PROBLEM
+
+'''Sir  python is not accepting any variable and and is constantly showing
+syntax error. But i am not able to understand why it is doing so while
+creating any variable. Because of this problem, I am also not able to create
+any heading also. I am getting this problem while making a biodata'''
+
+## HERE IS THE PROGRAMMING
+print{
+    'Vaani':{
+
+        'name' : 'Vaani Gupta'
+
+    }
+} 
+
+
+''' Sir please guide to where I am making a folly/mistake and how shall
+I proceed?'
