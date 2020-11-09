4aa734cde6db697502856589e85f725f1a361990
VAANI GUPTA <73345388+vaanigupta08@users.noreply.github.com>
2020-11-09 15:40:49 +0530

    Add files via upload

---
+++ DICTIONARY BIODATA.py
@@ -0,0 +1,19 @@
+# DICTIONARY BIODATA
+
+Bigdata = {
+    'Vaani' : {   
+    'name' : 'Vaani Gupta',
+    'course' : 'python',
+    'mobile' : 56411200024,
+    'email address' : '656232@hghguygy',
+    },
+    'Purva' : {
+        'name' : 'Purva',
+        'course' : 'ai',
+        'mobile' : 9899898932,
+        }
+}
+
+
+
+print(' Hello friends my name is {} and my email address is {}.'.format(Bigdata ['Vaani']['name'], Bigdata ['Vaani']['email address'] ))
