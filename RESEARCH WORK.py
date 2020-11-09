ec183b9440a9c7e8de22c2d5ec040fbe7db70ad3
VAANI GUPTA <73345388+vaanigupta08@users.noreply.github.com>
2020-11-09 15:35:23 +0530

    Add files via upload

---
+++ RESEARCH WORK.py
@@ -0,0 +1,45 @@
+# POP METHOD IN PYTHON
+
+'''pop() is an inbuilt function in Python that removes and returns
+ last value from the list or the given index value.
+ Parameter : index (optional)
+  - The value at index is popped out and removed.'''
+
+  # REMOVE METHOD IN PYTHON
+
+'''The remove() method removes the first matching element (which is passed as
+an argument)from the list.'''
+
+# WHY POP METHOD IS NOT GOOD FOR SET??
+
+'''The pop() method returns an arbitrary (random) element from the set.
+ Also, the set is updated and will not contain the element (which is returned).
+If the set is empty, TypeError exception is raised.'''
+
+# REMOVE METHOD IN PYTHON
+
+'''The remove() method removes the first matching element (which is passed as
+an argument)from the list.'''
+
+# DISCARD METHOD IN PYTHON
+
+'''The built-in method, discard() in Python, removes the element from the set
+ only if the element is present in the set. If the element is not present
+  in the set, then no error or exception is raised and the original set 
+  is printed.'''
+
+  # UNION METHOD IN PYTHON
+
+'''The Python set union() method returns a new set with distinct
+  elements from all the sets.The union of two or more sets is the set
+ of all distinct elements present in all the sets.'''
+
+ # UPDATE METHOD IN PYTHON
+
+'''The update() method updates the dictionary with the elements from
+ the another dictionary object or from an iterable of key/value pairs.
+Update() method adds element(s) to the dictionary if the key is not in
+ the dictionary. If the key is in the dictionary, it updates the key with
+  the new value.'''
+
+  
