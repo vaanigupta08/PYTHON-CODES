cffe8681814181434bb3fdc767a86d088b83323f
VAANI GUPTA <73345388+vaanigupta08@users.noreply.github.com>
2020-11-19 16:56:10 +0530

    Add files via upload

---
+++ WHILE LOOP.py
@@ -0,0 +1,39 @@
+
+# USING WHILE LOOP
+## PRINTING MESSAGE
+'''num2 = 1
+
+while num2 < 10:
+    print('\n hi, num2')
+    num2 += 1'''
+
+    ## PRINTING TABLE
+Harry = 4
+while Harry <= 40:
+    print(Harry)
+    Harry += 4
+
+# TAKING INPUTS FROM USER AND PRINTING TABLES 
+
+start = int(input('\n Please enter the starting number:'))
+end = int(input('\n Please enter the ending number:'))
+
+## CODE FOR PRINTING THE TABLE OF 1
+
+while start <= end:
+   print(start)
+   start += 1
+
+## CODE FOR PRINTING THE TABLE OF 2
+
+while start <= end:
+   print(start)
+   start += 2
+
+   ## PRINTING TABLES TAKING SINGLE INPUT FROM USER
+
+single = int(input('\n Please enter the number:'))
+
+while single <= 60:
+   print(single)
+   single += 6
